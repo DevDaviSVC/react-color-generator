156b742f8881d4f789923e80c5fb57950a71c106
DevDaviSVC <davisouzavieiradacosta@gmail.com>
2025-01-02 15:00:00 -0300

    Adding copy to clipboard

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,8 +1,13 @@
 const Color = ({color}) => {
   let hexColor = color.hexString();
   let textColor = color.type === "tint" || color.type === "base" ? "black" : "white";
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(hexColor);
+  }
+
   return (
-    <div className="color" style={{backgroundColor: hexColor, color: textColor}}>
+    <div className="color" style={{backgroundColor: hexColor, color: textColor}} onClick={handleClick}>
       <p>{color.weight}%</p>
       <p>{hexColor}</p>
     </div>
